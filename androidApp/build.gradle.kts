4835a489d0a6084739c5d6cadd3a65ceb184a19a
hiroaki404 <nosho404@gmail.com>
2025-01-02 18:12:38 +0900

    change application id

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.sample"
+        applicationId = "com.nhiroaki.sample"
         minSdk = 30
         targetSdk = 35
         versionCode = 1
