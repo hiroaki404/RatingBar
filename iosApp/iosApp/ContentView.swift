e0ead03c6d2a564d0d5931187f540ae0e1ff7c03
hiroaki404 <nosho404@gmail.com>
2025-01-03 10:19:14 +0900

    add ios project

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import compose
+
+struct ContentView: View {
+	var body: some View {
+		Text("hello")
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
